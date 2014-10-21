d6b3f16a770673893355f7789a16dcd0942a2f7d
olivierrr <oliviersinbox@gmail.com>
2014-10-21 16:29:26 -0700

    expanding schemas

--- server/models/breadstick.js
+++ server/models/breadstick.js
@@ -1,15 +1,33 @@
 var mongoose = require('mongoose')
 
+/**
+ * @schema
+ */
 var BreadStickSchema = mongoose.Schema({
 
-	source: String,
-    language: String,
-    users: [{id: String, hasCompleted: Boolean}],
+	// source should be a reference to somewhere else.
+	source: 	{type: String},
 
-    difficulty: Number,
-    likes: Number
+	title: 		{type: String},
+	author: 	{type: Number, ref: 'User'},
+    language: 	{type: String},
+    difficulty: {type: Number},
+    points:     {type: Number, default: 0},
+
+    // users that have tried/completed
+    users: 		[{
+    	id: {type: Number, ref: 'User'}, 
+    	hasCompleted: {type: Boolean, default: false}
+    }],
+
+    createdOn: 	{type: Date, default: Date.now}
+
 })
 
+/**
+ * @model
+ */
 var Breadstick = mongoose.model('BreadStick', BreadStickSchema)
+
 
 module.exports = Breadstick
--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,44 @@
 var mongoose = require('mongoose')
 var bcrypt = require('bcrypt')
 
+/**
+ * @schema
+ */
 var UserSchema = mongoose.Schema({
 
-    email: String,
-    username: String,
-    password: String,
-    createdOn: Date,
+    email:      String,
+    username:   String,
+    password:   String,
+    createdOn:  {type: Date, default: Date.now},
+    status:     {type: Number, default: 1},
 
-    points: Number,
+    points:     {type: Number, default: 0},
 
-    // TODO
-    breadsticks : [{id: String, hasCompleted: Boolean}]
+    breadsticks : [{
+        id: String, 
+        hasCompleted: {type: Boolean, default: false}
+    }]
+
 })
 
+/**
+ * @method
+ */
 UserSchema.methods.generateHash = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(), null)
 }
 
+/**
+ * @method
+ */
 UserSchema.methods.isValidPassword = function (password) {
     return bcrypt.compareSync(password, this.password)
 }
 
+/**
+ * @model
+ */
 var User = mongoose.model('User', UserSchema)
+
 
 module.exports = User
