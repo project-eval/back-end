43fb416550a92612336b85e2e002137e37e23e8b
olivierrr <oliviersinbox@gmail.com>
2014-10-28 04:49:18 -0700

    update coderunner

--- server/coderunner.js
+++ server/coderunner.js
@@ -1,20 +1,4 @@
 
-// var request = require('request')
-// module.exports = function (language, code) {
-
-// 	request.post({url:'http://localhost:9001/run', form: {l: language, c: code} }, 
-// 		function (error, response, body) {
-// 			if(error) console.log('coderunner error', error)
-// 			if (!error && response.statusCode == 200) {
-// 				return body
-// 			}
-// 	})
-// }
-// 
-// 
-
-// TEMP CODE RUNNER (only works with js)
-
 var SandBox = require('sandbox')
 var sandbox = new SandBox()
 
